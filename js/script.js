268eeb9d775420140876fef9b1861af508713bdd
Guidaí Carvallo <guidaicarvallo@gmail.com>
2023-08-26 12:28:28 -0300

    terminado

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+let arrayStrings = strangeArray.filter((elemento) => {
+  return typeof elemento === 'string';
+});
+
+let arrayFinal = arrayStrings.sort((a, b) => {
+  return a.localeCompare(b);
+});
+
+
+console.log(arrayStrings)
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  // Más info de forEach => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  // Escribe tu solución aquí
+  showList(arrayFinal);
+  // Sugerencia de cómo mostrar el array => showList(strangeArray);
+});
